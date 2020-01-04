df7ec59efb99cab16795f4d5c2593ffeecc6268f
Kent Chan <kntchn@gmail.com>
2020-01-04 03:26:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = data[:color].values.flatten
+  puts names
+
+
+  #find out the color
+  #data[:color].each do |key, hash|
+  #  hash.detect {|name|
+
+
 end
